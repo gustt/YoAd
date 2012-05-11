47c363efe145a4483b334c72b1defbda7c368bd6
Gustavo Neves Alves <gustavo.na@gmail.com>
2012-05-11 00:15:27 -0300

    Estrutura inicial, dll YoAd.MetadaManagar e UnitTest

---
+++ YoAd.MetadataManager/YoAd.MetadataManager/MetadataManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YoAd.MetadataManager
+{
+    public class MetadataManager
+    {
+        #region § construtor
+
+        private static readonly MetadataManager instance = new MetadataManager();
+
+        private MetadataManager() { }
+
+        public static MetadataManager go
+        {
+            get { return instance; }
+        } 
+
+        #endregion
+    }
+}
---
+++ YoAd.MetadataManager/YoAd.UnitTest/MetadataManagerConfigurationTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YoAd.MetadataManager;
+
+namespace YoAd.UnitTest
+{
+    [TestClass]
+    public class MetadataManagerConfigurationTest
+    {
+        [TestMethod]
+        public void InstanciaMetadataManager()
+        {
+            //if MetadataManager.MetadataManager
+        }
+    }
+}
